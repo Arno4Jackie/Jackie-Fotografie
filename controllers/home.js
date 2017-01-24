97167e1089a336b2a42cad8b151a4e313c9efaef
Arno Botha <bothab@verishare.co.za>
2017-01-24 14:20:02 +0200

    testing mongolab

--- controllers/home.js
+++ controllers/home.js
@@ -6,22 +6,22 @@
 
 exports.index = (req, res) => {
     console.log('test');
-    // photoCollection.findOne({
-    //     isMainImage: true
-    // }).exec(function(err, doc) {
-    //     if (err) throw err;
-    //     var imgUrl = doc.url;
+    photoCollection.findOne({
+        isMainImage: true
+    }).exec(function(err, doc) {
+        if (err) throw err;
+        var imgUrl = doc.url;
 
-    //     res.render('home', {
-    //         title: 'Jackie Fotografie',
-    //         imgUrl: imgUrl
-    //     });
-    // });
-
-    res.render('home', {
-        title: 'Jackie Fotografie',
-        imgUrl: 'https://docs.google.com/uc?id=0BydPt840pObUNG53dXd6M1BZQm8&amp;export=download'
+        res.render('home', {
+            title: 'Jackie Fotografie',
+            imgUrl: imgUrl
+        });
     });
+
+    // res.render('home', {
+    //     title: 'Jackie Fotografie',
+    //     imgUrl: 'https://docs.google.com/uc?id=0BydPt840pObUNG53dXd6M1BZQm8&amp;export=download'
+    // });
 };
 
 
