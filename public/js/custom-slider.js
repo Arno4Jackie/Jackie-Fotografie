9e7156132e6acc84d9ee8acd968e05d8238e1487
Arno Botha <bothab@verishare.co.za>
2017-05-08 14:30:23 +0200

    Finished Pricing slide logic and css for buttons

--- public/js/custom-slider.js
+++ public/js/custom-slider.js
@@ -1,4 +1,35 @@
 var CurrentImage = 1;
 
-var image = document.getElementById('PricingImage');
-image.src = "Pricing/1.jpg";
+document.getElementById('PricingImage').src = "Pricing/" + CurrentImage + ".jpg";
+disableButtons()
+
+function PreviousImage() {
+    if (parseInt(this.CurrentImage) > 1) {
+        this.CurrentImage--;
+        document.getElementById('PricingImage').src = "Pricing/" + this.CurrentImage + ".jpg";
+        disableButtons();
+    }
+}
+
+function NextImage() {
+    if (parseInt(this.CurrentImage) < 8) {
+        this.CurrentImage++;
+        document.getElementById('PricingImage').src = "Pricing/" + this.CurrentImage + ".jpg";
+        disableButtons();
+    }
+
+}
+
+function disableButtons() {
+    if (parseInt(this.CurrentImage) == 1) {
+        document.getElementById('btnPrev').disabled = true;
+    } else {
+        document.getElementById('btnPrev').disabled = false;
+    }
+
+    if (parseInt(this.CurrentImage) == 8) {
+        document.getElementById('btnNext').disabled = true;
+    } else {
+        document.getElementById('btnNext').disabled = false;
+    }
+}
